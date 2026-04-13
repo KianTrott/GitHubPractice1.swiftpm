92367aee081f6ca141ddd14770ec3ab19ea09f2c
Kian <ktrotter8454@stu.d214.org>
2026-04-13 11:06:44 -0500

    added button

--- ContentView.swift
+++ ContentView.swift
@@ -6,15 +6,39 @@ struct ContentView: View {
     @State var Result: Int = 0
     
     var body: some View {
-        HStack {
+        VStack{
+            HStack(alignment: .center) {
+                
+                
+                TextField("Enter First Number", value: $FirstNumber, format: .number)
+                    .textFieldStyle(.roundedBorder)
+                
+                Text("+")
+                
+                TextField("Enter Second Number", value: $SecondNumber, format: .number)
+                    .textFieldStyle(.roundedBorder)
+                
+                
+            }
             
+            Button {
+                Result = FirstNumber + SecondNumber
+            } label: {
+                ZStack{
+                    RoundedRectangle(cornerRadius: 30)
+                        .frame(width: 220, height: 130)
+                        .foregroundColor(.blue)
+                    Text("Calculate")
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundStyle(.white)
+                    
+                    
+                }
+                
+                }
             
-            TextField("Enter First Number", value: $FirstNumber, format: .number)
-            
-            
-            
-            TextField("Enter Second Number", value: $SecondNumber, format: .number)
-            
+
         }
     }
 }
